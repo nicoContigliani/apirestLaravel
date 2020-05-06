866d6ccaf7ccb0d3124f405ad27e62be38cb176f
nicoContigliani <nico.contigliani@gmail.com>
2020-05-05 23:45:48 -0300

    first commit

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tasks as AppTasks;
+use Illuminate\Http\Request;
+use App\Tasks;
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $tasks = Tasks::all();
+        return $tasks;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $tasks = Tasks::create($request->all());
+        return $tasks;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $tasks = Tasks::find($id);
+        return $tasks;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id, Request $request)
+    {
+        //
+     
+       }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    
+        $tasks = $this->show($id);
+        $tasks->fill($request->all())->save();
+        return $tasks; 
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id,Request $request)
+    {
+        //
+        $tasks=$this->show($id);
+        $tasks->delete();   
+        return $tasks;
+    }
+}
